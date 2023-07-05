ce73003c088000f18bb31547f116bfe246d26391
Zach-Trani <trani.zach@gmail.com>
2023-07-05 19:43:12 -0400

    fix: add documentation to the footer component

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -1,5 +1,10 @@
 import "./Footer.css"
 
+/**
+ * The Footer component renders the footer of the page and website design by which is typical.
+ *
+ * @return {*}
+ */
 export const Footer = () => {
     return (
         <div className="designed-by d-flex justify-content-center">
