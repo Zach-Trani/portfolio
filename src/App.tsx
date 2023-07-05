3c7fda852a4e67ef3626c30101000da2b5ce5f7a
Zach-Trani <trani.zach@gmail.com>
2023-07-05 19:39:04 -0400

    feat: add footer skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar } from "./Components/Layout/Navbar";
 import { Greeting } from "./Components/Layout/Greeting";
+import { Footer } from "./Components/Layout/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Greeting />
+      <Footer />
     </div>
   );
 }
