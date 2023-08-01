149c43a18fa2c0cac9aedbcac7ed21730ef2a247
Zach-Trani <trani.zach@gmail.com>
2023-08-01 15:25:21 -0400

    Add skeleton for contact-component

--- src/Components/Layout/Contact.tsx
+++ src/Components/Layout/Contact.tsx
@@ -1,12 +1,22 @@
+import "./Contact.css"
+
 export const Contact = () => {
     return (
         <div className="contact-container">
-            <form className="d-flex flex-column">
+            <div className="contact">
+            <form>
                 <h3>Let's Talk</h3>
                 <input type="text" placeholder="Name"></input>
                 <input type="text" placeholder="Email"></input>
-                <textarea rows={4}></textarea>
+                <textarea placeholder="Message" rows={4}></textarea>
             </form>
+            </div>
         </div>
     )
 }
+
+// <div>
+//             <div className="d-flex justify-content-center align-items-end credit-container">
+//             Website Design by Zachary Trani ©2023
+//             </div>
+//         </div>
